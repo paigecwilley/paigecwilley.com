aa5e67fd27dadbd957e3371655e76da5b6469499
Paige Willey <paigewilley@Paiges-MacBook-Pro-2.local>
2020-12-31 20:05:14 -0700

    hotfix of tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.js", "./components/**/*.js"],
+  purge: ["./pages/**/**/*.js", "./components/**/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
